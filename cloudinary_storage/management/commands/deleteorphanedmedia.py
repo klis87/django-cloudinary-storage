b5cccc96f1f31a35696017b805a50cf47af1575e
Konrad Lisiczynski <klisiczynski@gmail.com>
2016-08-06 13:53:29 +0200

    Added get_uploaded_media method to delete media command

--- cloudinary_storage/management/commands/deleteorphanedmedia.py
+++ cloudinary_storage/management/commands/deleteorphanedmedia.py
@@ -1,3 +1,5 @@
+from itertools import chain
+
 from django.core.management.base import BaseCommand
 from django.apps import apps
 from django.db import models
@@ -8,12 +10,30 @@ from cloudinary_storage import app_settings
 class Command(BaseCommand):
     help = 'Removes all orphaned media files'
 
+    def models(self):
+        return apps.get_models()
+
+    def model_file_fields(self, model):
+        for field in model._meta.fields:
+            if isinstance(field, models.FileField):
+                yield field
+
     def get_resource_types(self):
         media_types = set()
-        model_list = apps.get_models()
-        for model in model_list:
-            for field in model._meta.fields:
-                if isinstance(field, models.FileField):
-                    media_type = field.storage.RESOURCE_TYPE
-                    media_types.add(media_type)
+        for model in self.models():
+            for field in self.model_file_fields(model):
+                media_type = field.storage.RESOURCE_TYPE
+                media_types.add(media_type)
         return media_types
+
+    def get_uploaded_media(self):
+        uploaded_media = []
+        for model in self.models():
+            media_fields = []
+            for field in self.model_file_fields(model):
+                media_fields.append(field.name)
+            if media_fields:
+                exclude_options = {media_field: '' for media_field in media_fields}
+                model_uploaded_media = model.objects.exclude(**exclude_options).values_list(*media_fields)
+                uploaded_media.extend(model_uploaded_media)
+        return set(chain.from_iterable(uploaded_media))
--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,9 +1,45 @@
-from django.test import SimpleTestCase
+from django.test import TestCase
+from django.core.files.base import ContentFile
+from django.core.files.images import ImageFile
 
 from cloudinary_storage.management.commands.deleteorphanedmedia import Command as DeleteOrphanedMediaCommand
+from cloudinary_storage.storage import MediaCloudinaryStorage, RawMediaCloudinaryStorage
+from tests.models import TestModel, TestImageModel, TestModelWithoutFile
+from tests.test_helpers import get_random_name
 
 
-class DeleteOrphanedMediaCommandTests(SimpleTestCase):
+class DeleteOrphanedMediaCommandTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        TestModelWithoutFile.objects.create(name='without file')
+        TestModel.objects.create(name='without file')
+        TestImageModel.objects.create(name='without image')
+        content = ContentFile(b'Content of file')
+        file_model = TestModel(name='with file')
+        file_model.file.save(get_random_name(), content)
+        cls.file_name = file_model.file.name
+        file_model = TestModel(name='with file')
+        file_model.file.save(get_random_name(), content)
+        cls.file_name_2 = file_model.file.name
+        image = ImageFile(open('tests/dummy-image.jpg', 'rb'))
+        image_model = TestImageModel(name='with image')
+        image_model.file.save(get_random_name(), image)
+        cls.file_name_3 = image_model.file.name
+
     def test_get_resource_types(self):
         command = DeleteOrphanedMediaCommand()
         self.assertEqual({'raw', 'image'}, command.get_resource_types())
+
+    def test_get_uploaded_media(self):
+        command = DeleteOrphanedMediaCommand()
+        self.assertEqual(command.get_uploaded_media(), {self.file_name, self.file_name_2, self.file_name_3})
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        raw_storage = RawMediaCloudinaryStorage()
+        raw_storage.delete(cls.file_name)
+        raw_storage.delete(cls.file_name_2)
+        image_storage = MediaCloudinaryStorage()
+        image_storage.delete(cls.file_name_3)
