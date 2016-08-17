1c83ef6c9e99bce2b36992b7cd6aac1c7b42d2bf
Konrad Lisiczynski <klisiczynski@gmail.com>
2016-08-17 12:10:22 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 from setuptools import setup
 
+description = ('Django package that provides Cloudinary storages for both media and static files '
+               'as well as management commands for removing unnecessary files.')
+
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
     name='django-cloudinary-storage',
-    version='1.0',
+    version='0.1',
     author='Konrad Lisiczynski',
     author_email='klisiczynski@gmail.com',
+    description=description,
+    long_description=long_description,
+    licence='MIT',
+    url='https://github.com/klis87/django-cloudinary-storage#how-to-run-tests',
+    keywords='django cloudinary storage',
     packages=['cloudinary_storage'],
+    include_package_data=True,
     install_requires=[
         'requests>=2.10.0',
         'cloudinary>=1.4.0'
