b8cabd2ebbf67b9cfbbf4defee1a750fea5950a9
klis87 <klisiczynski@gmail.com>
2017-12-13 11:54:29 +0100

    v 0.2.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = ('Django Cloudinary Storage is a Django package that facilita
 
 setup(
     name='django-cloudinary-storage',
-    version='0.2.2',
+    version='0.2.3',
     author='Konrad Lisiczynski',
     author_email='klisiczynski@gmail.com',
     description=description,
