3ad274073fe8a9ce5f27bf2d73b1bf680540199d
Konrad <klisiczynski@gmail.com>
2020-08-24 00:23:03 +0200

    Updated package version to 0.3.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='django-cloudinary-storage',
-    version='0.2.3',
+    version='0.3.0',
     author='Konrad Lisiczynski',
     author_email='klisiczynski@gmail.com',
     description=description,
